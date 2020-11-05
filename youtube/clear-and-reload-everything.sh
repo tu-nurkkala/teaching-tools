7152e5923b264e8b17a7ccec76736b2ddabe6fe1
Tom Nurkkala <tnurkkala@cse.taylor.edu>
2020-11-05 10:01:34 -0500

    Minor update

--- youtube/clear-and-reload-everything.sh
+++ youtube/clear-and-reload-everything.sh
@@ -7,3 +7,4 @@ SQL_FILE_NAME="insert-all-$NOW.sql"
 node fetch-metadata.js > $SQL_FILE_NAME
 psql --file=delete-all-data.sql $DB_NAME 
 psql --file=$SQL_FILE_NAME $DB_NAME 
+psql --csv --file=all-videos.sql $DB_NAME > all-videos.csv
