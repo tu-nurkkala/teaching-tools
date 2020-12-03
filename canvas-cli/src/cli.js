313dfde1b0109191041277028a3e5c3837bdc1aa
Tom Nurkkala <tnurkkala@cse.taylor.edu>
2020-12-03 11:49:44 -0500

    Checkpoint WIP

--- canvas-cli/src/cli.js
+++ canvas-cli/src/cli.js
@@ -1,9 +1,7 @@
-
-
-// const { program } = require("commander");
-//
-// program.version("0.0.1");
+import { program } from "commander";
+import { version } from "../package.json";
 
 export function cli(args) {
-  console.log(args);
+  program.version(version);
+  program.parse(args);
 }
